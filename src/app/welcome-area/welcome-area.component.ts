479ed3341478d8304a4581a258b961e1cfda849b
M-Nafi <nafi-m@gmx.de>
2024-09-14 12:30:25 +0200

    first commit. added first components

---
+++ src/app/welcome-area/welcome-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-area.component.html',
+  styleUrl: './welcome-area.component.scss'
+})
+export class WelcomeAreaComponent {
+
+}
