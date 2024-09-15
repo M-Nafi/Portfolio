6c523a2fc277a90826a7f2212c103b582a8f771b
M-Nafi <nafi-m@gmx.de>
2024-09-15 15:31:31 +0200

    some visual improvements made in header-component and welcome-area-component

--- src/app/welcome-area/welcome-area.component.ts
+++ src/app/welcome-area/welcome-area.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-area',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './welcome-area.component.html',
-  styleUrl: './welcome-area.component.scss'
+  styleUrl: './welcome-area.component.scss',
 })
 export class WelcomeAreaComponent {
-
+  items = [
+    'Frontend Developer',
+    'Open to work',
+    'Based in Bremen',    
+    'Available for remote work',
+  ];
 }
