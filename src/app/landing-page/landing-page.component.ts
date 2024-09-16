0f2279d69b46f3f67eb94984580da6e7ae2a2c88
M-Nafi <nafi-m@gmx.de>
2024-09-16 21:16:08 +0200

    added about-me component. still needs change the main-picture and finishing touches. not yet finished

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { WelcomeAreaComponent } from '../welcome-area/welcome-area.component';
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, WelcomeAreaComponent],
+  imports: [CommonModule, WelcomeAreaComponent, AboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
