b35b354f529fd624c005ba0814f925e154971c9f
Nafi M <nafim@MacBook-Air-von-Nafi.fritz.box>
2024-09-18 21:17:56 +0200

    added featured-projects-component. added first html and css code. not finished yet. still need to add animations

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { WelcomeAreaComponent } from '../welcome-area/welcome-area.component';
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { SkillSetComponent } from '../skill-set/skill-set.component';
+import { FeaturedProjectsComponent } from '../featured-projects/featured-projects.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, WelcomeAreaComponent, AboutMeComponent, SkillSetComponent],
+  imports: [CommonModule, WelcomeAreaComponent, AboutMeComponent, SkillSetComponent, FeaturedProjectsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
