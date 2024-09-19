b7a3274c495131f230741e95dc7defd110a13a71
Nafi M <nafim@7a86a451-274c-43fb-a377-f8b8b62b8419.fritz.box>
2024-09-19 09:46:53 +0200

    working on feedback part

---
+++ src/app/colleagues-feedback/colleagues-feedback.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colleagues-feedback',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './colleagues-feedback.component.html',
+  styleUrl: './colleagues-feedback.component.scss'
+})
+
+export class ColleaguesFeedbackComponent {
+  
+}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { WelcomeAreaComponent } from '../welcome-area/welcome-area.component';
-import { AboutMeComponent } from "../about-me/about-me.component";
+import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillSetComponent } from '../skill-set/skill-set.component';
 import { FeaturedProjectsComponent } from '../featured-projects/featured-projects.component';
+import { ColleaguesFeedbackComponent } from '../colleagues-feedback/colleagues-feedback.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, WelcomeAreaComponent, AboutMeComponent, SkillSetComponent, FeaturedProjectsComponent],
+  imports: [
+    CommonModule,
+    WelcomeAreaComponent,
+    AboutMeComponent,
+    SkillSetComponent,
+    FeaturedProjectsComponent,
+    ColleaguesFeedbackComponent,
+  ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
