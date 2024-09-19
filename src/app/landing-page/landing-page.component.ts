c5072865cf3a9b2349b60016046943a251bb8705
M-Nafi <nafi-m@gmx.de>
2024-09-19 14:03:28 +0200

    work-together-component optically adapted. functions are not yet given. not finished yet

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -5,6 +5,7 @@ import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillSetComponent } from '../skill-set/skill-set.component';
 import { FeaturedProjectsComponent } from '../featured-projects/featured-projects.component';
 import { ColleaguesFeedbackComponent } from '../colleagues-feedback/colleagues-feedback.component';
+import { WorkTogetherComponent } from '../work-together/work-together.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -16,6 +17,7 @@ import { ColleaguesFeedbackComponent } from '../colleagues-feedback/colleagues-f
     SkillSetComponent,
     FeaturedProjectsComponent,
     ColleaguesFeedbackComponent,
+    WorkTogetherComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
--- src/app/work-together/work-together.component.ts
+++ src/app/work-together/work-together.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work-together',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work-together.component.html',
   styleUrl: './work-together.component.scss'
 })
