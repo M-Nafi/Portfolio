5d76188e04cdd1124a243ae2181fbc97f85a2647
M-Nafi <nafi-m@gmx.de>
2024-09-19 11:42:56 +0200

    added work-together-component and feedback area is displayed visually. function is not yet available

--- src/app/colleagues-feedback/colleagues-feedback.component.ts
+++ src/app/colleagues-feedback/colleagues-feedback.component.ts
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './colleagues-feedback.component.html',
-  styleUrl: './colleagues-feedback.component.scss'
+  styleUrl: './colleagues-feedback.component.scss',
 })
 
 export class ColleaguesFeedbackComponent {
-  
+  feedback = [
+    { text: 'Our project benefited enormously from Lukas efficient way of working.' },
+    { text: 'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project. ' },
+    { text: 'I had the good fortune of working with Simon in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He is super knowledgeable, easy to work with, and I would happily work with him again given the chance.' },
+  ];
+
+  onFeedbackClick(feedback: any) {
+    console.log('Clicked feedback', feedback);
+  }
 }
