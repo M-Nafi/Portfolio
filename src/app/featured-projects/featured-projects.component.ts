3ab94c39ab6e92b8179165cf5a46c4fe65168c99
M-Nafi <nafi-m@gmx.de>
2024-09-27 08:43:22 +0200

    featured-projects-component images added, which will displayed animated when hovering

--- src/app/featured-projects/featured-projects.component.ts
+++ src/app/featured-projects/featured-projects.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './featured-projects.component.html',
-  styleUrl: './featured-projects.component.scss'
+  styleUrls: ['./featured-projects.component.scss']
 })
 export class FeaturedProjectsComponent {
-
+  activeImage: string | null = null;
+  
+  showImage(project: string) {
+    this.activeImage = project;
+  }
+ 
+  hideImage() {
+    this.activeImage = null;
+  }
 }
