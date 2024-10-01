53796e578acb0823e5d3ae83205d426a77efc3ee
Nafi M <nafim@MacBook-Air-von-Nafi.local>
2024-10-02 00:28:43 +0200

    working on translation of whole project. finished header-component translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
-import { FooterComponent } from "./shared/footer/footer.component";
-import { WelcomeAreaComponent } from "./welcome-area/welcome-area.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { WelcomeAreaComponent } from './welcome-area/welcome-area.component';
+import { CommonModule } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeAreaComponent],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeAreaComponent,
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router,
+    translate: TranslateService
+  ) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,51 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  currentLanguage: string = 'EN'; 
+  currentLanguage: string = 'EN';
 
-  translations: {
-    [ key: string ]: 
-    {
-      aboutMe: string;
-      skills: string;
-      projects: string;
-    }
-  } = {
-    EN: 
-    {
-      aboutMe: 'About me',
-      skills: 'Skills',
-      projects: 'Projects'
-    },
-    DE: 
-    {
-      aboutMe: 'Über mich',
-      skills: 'Fähigkeiten',
-      projects: 'Projekte'
-    }
-  };
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = translate.currentLang?.toUpperCase() || 'EN'; // beginnt immer mit en. also standart
+  }
 
   switchLanguage() {
     this.currentLanguage = this.currentLanguage === 'DE' ? 'EN' : 'DE';
-  }
-
-  get aboutMe() {
-    return this.translations[this.currentLanguage].aboutMe;
-  }
-
-  get skills() {
-    return this.translations[this.currentLanguage].skills;
-  }
-
-  get projects() {
-    return this.translations[this.currentLanguage].projects;
+    this.translate.use(this.currentLanguage.toLowerCase()); 
   }
 }
