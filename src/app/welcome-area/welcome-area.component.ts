e0eabfb1af96af24a85047806073cc08362dec31
Nafi M <nafim@MacBook-Air-von-Nafi.local>
2024-10-02 01:22:34 +0200

    working on translation on welcome-ares-component

--- src/app/welcome-area/welcome-area.component.ts
+++ src/app/welcome-area/welcome-area.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-welcome-area',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './welcome-area.component.html',
   styleUrl: './welcome-area.component.scss',
 })
