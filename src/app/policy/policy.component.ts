fb855cfd09a82bc947fcabdfa095ac0de1032b9a
M-Nafi <nafi-m@gmx.de>
2024-10-07 10:54:47 +0200

    made some visual improvements for mobile view. added links for websites

--- src/app/policy/policy.component.ts
+++ src/app/policy/policy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-policy',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './policy.component.html',
   styleUrl: './policy.component.scss'
 })
