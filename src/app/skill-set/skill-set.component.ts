d20a3f285c0308820143d4c51fb7fc0cfbc7937e
M-Nafi <nafi-m@gmx.de>
2024-10-08 11:58:10 +0200

    working on responsive design in whole project

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -14,9 +14,9 @@ export class SkillSetComponent {
   skills = [
     { src: 'assets/img/1-html-icon.png', label: 'HTML' },
     { src: 'assets/img/2-css-icon.png', label: 'CSS' },
-    { src: 'assets/img/3-javascript-icon.png', label: 'JavaScript' },
+    { src: 'assets/img/3-javascript-icon.png', label: 'Java Script' },
     { src: 'assets/img/4-materialdesign-icon.png', label: 'Material Design' },
-    { src: 'assets/img/5-typescript-icon.png', label: 'TypeScript' },
+    { src: 'assets/img/5-typescript-icon.png', label: 'Type Script' },
     { src: 'assets/img/6-angular-icon.png', label: 'Angular' },
     { src: 'assets/img/7-firebase-icon.png', label: 'Firebase' },
     { src: 'assets/img/8-git-icon.png', label: 'Git' },
