9451ea5ea8891928b1de86facb61201be39c0c24
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-08-01 01:41:30 +0200

    WIP: vor Angular Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TranslateService } from '@ngx-translate/core';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
